2fe5796bc4b9032ac5a2aa7431e5c7e19f251ad8
thomasyous <thomasyus@yahoo.com>
2024-09-06 19:59:47 -0400

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "tskbuckets"
+    bucket         = "buckertskg"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
